b370387e45e99cc3028a4b69bdfe24051c120593
Vivek Hariharan <vivekhariharan11@gmail.com>
2012-09-06 14:50:05 -0500

    connected layout with activity. Modified layouts a little

--- TDApp/src/com/vh/tdapp/activities/EditTaskActivity.java
+++ TDApp/src/com/vh/tdapp/activities/EditTaskActivity.java
@@ -1,7 +1,17 @@
 package com.vh.tdapp.activities;
 
+import com.vh.tdapp.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class EditTaskActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_task_layout);
+	}
 
 }
--- TDApp/src/com/vh/tdapp/activities/ShowTasksActivity.java
+++ TDApp/src/com/vh/tdapp/activities/ShowTasksActivity.java
@@ -1,7 +1,21 @@
 package com.vh.tdapp.activities;
 
+import com.vh.tdapp.R;
+import com.vh.tdapp.db.TasksDbAdapter;
+
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ShowTasksActivity extends Activity{
+	
+	TasksDbAdapter tasksAdapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		tasksAdapter = new TasksDbAdapter(this);
+		setContentView(R.layout.show_tasks_layout);
+	}
 
 }
