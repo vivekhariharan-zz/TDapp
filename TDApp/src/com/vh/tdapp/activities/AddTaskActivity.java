d589b5f6547e14d5610814c7df68e6c5f95ff68a
Vivek Hariharan <vivekhariharan11@gmail.com>
2012-09-06 16:36:54 -0500

    added activity that is used to handle getting new tasks

---
+++ TDApp/src/com/vh/tdapp/activities/AddTaskActivity.java
@@ -0,0 +1,17 @@
+package com.vh.tdapp.activities;
+
+import com.vh.tdapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddTaskActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_task_layout);
+		
+	}
+
+}
